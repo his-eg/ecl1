74dacb818cd50ebb23bd78629853a1a2b0834ca2
Hendrik Brummermann <nhnb@users.sourceforge.net>
2012-06-15 14:52:38 +0200

    code cleanup

--- src/de/his/cs/sys/extensions/Activator.java
+++ src/de/his/cs/sys/extensions/Activator.java
@@ -14,11 +14,6 @@ public class Activator extends AbstractUIPlugin {
 	/** The shared instance */
 	private static Activator plugin;
 
-	/**
-	 * The constructor
-	 */
-	public Activator() {
-	}
 
 	/**
 	 * starts the plugin
