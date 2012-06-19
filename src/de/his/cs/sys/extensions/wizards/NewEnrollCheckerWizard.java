ce04eeaec21891ed2e2993606c351623b97c11ca
Hendrik Brummermann <nhnb@users.sourceforge.net>
2012-06-19 18:28:08 +0200

    write entry in spring.xml

--- src/de/his/cs/sys/extensions/wizards/NewEnrollCheckerWizard.java
+++ src/de/his/cs/sys/extensions/wizards/NewEnrollCheckerWizard.java
@@ -9,11 +9,18 @@ import java.io.InputStreamReader;
 import java.io.PrintStream;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.dialogs.WizardNewFileCreationPage;
+import org.eclipse.ui.model.IWorkbenchAdapter;
 
 import de.his.cs.sys.extensions.wizards.utils.ResourceSupport;
 
@@ -25,8 +32,7 @@ import de.his.cs.sys.extensions.wizards.utils.ResourceSupport;
 public class NewEnrollCheckerWizard extends Wizard implements INewWizard {
 
 	private IStructuredSelection selection;
-	private NewEnrollCheckerWizardPage	 newFileWizardPage;
-	private IWorkbench workbench;
+	private NewEnrollCheckerWizardPage newFileWizardPage;
 
 	/**
 	 * creates a wizard for new EnrollChecks
@@ -37,61 +43,146 @@ public class NewEnrollCheckerWizard extends Wizard implements INewWizard {
 
 	@Override
 	public void addPages() {
-	    newFileWizardPage = new NewEnrollCheckerWizardPage(selection);
-	    addPage(newFileWizardPage);
+		newFileWizardPage = new NewEnrollCheckerWizardPage(selection);
+		addPage(newFileWizardPage);
 	}
 
 	@Override
 	public boolean performFinish() {
 
-	    IFile file = newFileWizardPage.createNewFile();
-	    if (file != null) {
+		IFile file = newFileWizardPage.createNewFile();
+		if (file != null) {
 			return true;
 		} else {
 			return false;
 		}
 	}
 
+	@SuppressWarnings("hiding")
 	@Override
 	public void init(IWorkbench workbench, IStructuredSelection selection) {
-	    this.workbench = workbench;
-	    this.selection = selection;
+		this.selection = selection;
 	}
 
 	class NewEnrollCheckerWizardPage extends WizardNewFileCreationPage {
+		private final IProject project;
 
-	    public NewEnrollCheckerWizardPage (IStructuredSelection selection) {
-	        super("NewConfigFileWizardPage", selection);
-	        setTitle("New EnrollChecker");
-	        setDescription("Creates a new EnrollChecker ");
-	        setFileExtension("java");
-	    }
+		public NewEnrollCheckerWizardPage(IStructuredSelection selection) {
+			super("NewConfigFileWizardPage", selection);
+			setTitle("New EnrollChecker");
+			setDescription("Creates a new EnrollChecker ");
+			setFileExtension("java");
+			this.project = convertSelection(selection);
+		}
 
-	    @Override
-	    protected InputStream getInitialContents() {
+		@Override
+		protected InputStream getInitialContents() {
 
-	    	// guess package and name
-	    	String name = this.getFileName().replace(".java", "");
-	    	String packageName = this.getContainerFullPath().toPortableString();
-	    	int pos = packageName.indexOf("/src/java/");
-	    	packageName = packageName.substring(pos + 10).replace('/', '.');
+			// guess package and name
+			String name = this.getFileName().replace(".java", "");
+			String packageName = this.getContainerFullPath().toPortableString();
+			int pos = packageName.indexOf("/src/java/");
+			packageName = packageName.substring(pos + 10).replace('/', '.');
 
-	    	// write file with replaced variables
-	    	ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-	    	PrintStream writer = new PrintStream(buffer);
-	    	InputStream is = ResourceSupport.class.getResourceAsStream("templates/src/java/EnrollChecker.java.template");
-	    	BufferedReader br = new BufferedReader(new InputStreamReader(is));
-	    	try {
-		    	String line = br.readLine();
-		    	while (line != null) {
-		    		String temp = line.replace("[name]", name).replace("[package]", packageName);
-		    		writer.println(temp);
-		    		line = br.readLine();
-		    	}
-	    	} catch (IOException e) {
-	    		e.printStackTrace();
-	    	}
-	    	return new ByteArrayInputStream(buffer.toByteArray());
-	    }
+			writeSpringEntry(packageName, name);
+
+			// write file with replaced variables
+			ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+			PrintStream writer = new PrintStream(buffer);
+			InputStream is = ResourceSupport.class
+					.getResourceAsStream("templates/src/java/EnrollChecker.java.template");
+			BufferedReader br = new BufferedReader(new InputStreamReader(is));
+			try {
+				String line = br.readLine();
+				while (line != null) {
+					String temp = line.replace("[name]", name).replace("[package]", packageName);
+					writer.println(temp);
+					line = br.readLine();
+				}
+				br.close();
+				writer.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return new ByteArrayInputStream(buffer.toByteArray());
+		}
+
+		private void writeSpringEntry(String packageName, String name) {
+			String firstLowercaseName = firstCharLowerCase(name);
+			String sep = System.getProperty("line.separator");
+			String newEntry =
+					"		<bean id=\"" + firstLowercaseName + "\"" + sep +
+					"		  class=\"" + packageName + "." + name + "\"" + sep +
+					"		  scope=\"prototype\">" + sep +
+					"		  <property name=\"planelementDao\" ref=\"planelementDao\"/>" + sep +
+					"		  <property name=\"personDao\" ref=\"personDao\"/>" + sep +
+					"		</bean>" + sep;
+
+			IFile file = project.getFile("/src/java/extension.beans.spring.xml");
+			try {
+				InputStream is = file.getContents();
+				BufferedReader br = new BufferedReader(new InputStreamReader(is));
+				ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+				PrintStream writer = new PrintStream(buffer);
+
+				String line = br.readLine();
+				while (line != null) {
+					if (line.trim().equals("</beans>")) {
+						writer.println(newEntry);
+					}
+					writer.println(line);
+					line = br.readLine();
+				}
+				br.close();
+				writer.close();
+				file.setContents(new ByteArrayInputStream(buffer.toByteArray()), true, true, null);
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (CoreException e) {
+				e.printStackTrace();
+			}
+		}
+
+		/**
+		 * converts the first character to lower case
+		 *
+		 * @param name name
+		 * @return first character in lower case
+		 */
+		private String firstCharLowerCase(String name) {
+			if ((name == null) || name.equals("")) {
+				return "";
+			} else if (name.length() == 1) {
+				return name.toLowerCase();
+			}
+			return name.substring(0, 1).toLowerCase() + name.substring(1);
+		}
+
+		// http://stackoverflow.com/a/10970127 by mchr
+		private IProject convertSelection(IStructuredSelection structuredSelection) {
+			IProject res = null;
+			Object element = structuredSelection.getFirstElement();
+
+			if (element instanceof IResource) {
+				res = ((IResource) element).getProject();
+			} else if (element instanceof IJavaElement) {
+				IJavaElement javaElement = (IJavaElement) element;
+				res = javaElement.getJavaProject().getProject();
+			} else if (element instanceof IAdaptable) {
+				IAdaptable adaptable = (IAdaptable) element;
+				IWorkbenchAdapter adapter = (IWorkbenchAdapter) adaptable
+						.getAdapter(IWorkbenchAdapter.class);
+				if (adapter != null) {
+					Object parent = adapter.getParent(adaptable);
+					if (parent instanceof IJavaProject) {
+						IJavaProject javaProject = (IJavaProject) parent;
+						res = javaProject.getProject();
+					}
+				}
+			}
+
+			return res;
+		}
+
 	}
 }
