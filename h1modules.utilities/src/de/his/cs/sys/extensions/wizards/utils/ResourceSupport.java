e0fea0075fd50cdd90157a9cdac2c98b9cedfde2
Markus Keunecke <markuskeunecke@users.sourceforge.net>
2012-07-02 11:16:33 +0200

    organize imports

--- h1modules.utilities/src/de/his/cs/sys/extensions/wizards/utils/ResourceSupport.java
+++ h1modules.utilities/src/de/his/cs/sys/extensions/wizards/utils/ResourceSupport.java
@@ -1,13 +1,9 @@
 package de.his.cs.sys.extensions.wizards.utils;
 
-import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
