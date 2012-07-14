fb0e89ce13918e518c4c0b80e741462ea55003b8
Markus Keunecke <markuskeunecke@users.sourceforge.net>
2012-07-14 18:37:11 +0200

    add strategies for h1 and h1 extensions

---
+++ h1modules/src/de/his/cs/sys/extensions/wizards/pages/ExtensionStrategy.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2012 HIS GmbH All Rights Reserved.
+ *
+ * $Id$
+ *
+ * $Log$
+ *
+ * Created on 14.07.2012 by keunecke
+ */
+package de.his.cs.sys.extensions.wizards.pages;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+/**
+ * @author keunecke
+ * @version $Revision$ 
+ */
+public class ExtensionStrategy implements PackageStructureStrategy {
+
+	/* (non-Javadoc)
+	 * @see de.his.cs.sys.extensions.wizards.pages.PackageStructureStrategy#packagesToCreate(java.lang.String)
+	 */
+	@Override
+	public Collection<String> packagesToCreate(String project) {
+		return Arrays.asList("de.his.extensions." + project);
+	}
+
+}
---
+++ h1modules/src/de/his/cs/sys/extensions/wizards/pages/HISinOneStrategy.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2012 HIS GmbH All Rights Reserved.
+ *
+ * $Id$
+ *
+ * $Log$
+ *
+ * Created on 14.07.2012 by keunecke
+ */
+package de.his.cs.sys.extensions.wizards.pages;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * @author keunecke
+ * @version $Revision$ 
+ */
+public class HISinOneStrategy implements PackageStructureStrategy {
+	
+	private static final String[] prefixes = {"de.his.appserver.model.", 
+												"de.his.appserver.service.iface.", 
+												"de.his.appserver.service.impl.",
+												"de.his.appserver.persistence.iface.",
+												"de.his.appserver.persistence.impl.hibernate.",
+												"de.his.appclient.jsf"};
+
+	@Override
+	public Collection<String> packagesToCreate(String project) {
+		Collection<String> result = new ArrayList<String>();
+		for (String prefix : prefixes) {
+			result.add(prefix + project);
+		}
+		return result;
+	}
+
+}
