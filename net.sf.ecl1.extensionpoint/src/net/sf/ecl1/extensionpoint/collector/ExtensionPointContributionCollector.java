64d0acbaca3ec0cafbb1b379e5638ee9709cba7e
Markus Keunecke <markuskeunecke@users.sourceforge.net>
2012-08-22 15:24:20 +0200

    javadoc

--- net.sf.ecl1.extensionpoint/src/net/sf/ecl1/extensionpoint/collector/ExtensionPointContributionCollector.java
+++ net.sf.ecl1.extensionpoint/src/net/sf/ecl1/extensionpoint/collector/ExtensionPointContributionCollector.java
@@ -1,11 +1,19 @@
 package net.sf.ecl1.extensionpoint.collector;
 
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.compiler.CompilationParticipant;
 
+/**
+ * This class collects all contributions to extension points from projects, that have a file
+ * extension.ant.properties in their project root.
+ * 
+ * @author keunecke
+ *
+ */
 public class ExtensionPointContributionCollector extends CompilationParticipant {
+
+	private static final String EXTENSION_ANT_PROPERTIES = "extension.ant.properties";
 
 	@Override
 	public int aboutToBuild(IJavaProject project) {
@@ -14,14 +22,14 @@ public class ExtensionPointContributionCollector extends CompilationParticipant 
 
 	@Override
 	public void buildFinished(IJavaProject project) {
-		// TODO Auto-generated method stub
+		// TODO 
 		super.buildFinished(project);
 	}
 
 	@Override
 	public boolean isActive(IJavaProject project) {
 		boolean containsExtensionAntProperties = false;
-		IFile file = project.getProject().getFile("extension.ant.properties");
+		IFile file = project.getProject().getFile(EXTENSION_ANT_PROPERTIES);
 		containsExtensionAntProperties = file.exists();
 		return containsExtensionAntProperties;
 	}
