25e54cb253db853bae416446910e8d92a4f1ab46
Markus Keunecke <markuskeunecke@users.sourceforge.net>
2012-08-23 18:13:36 +0200

    add a utility class for getting certain objects from a java project

---
+++ net.sf.ecl1.extensionpoint/src/net/sf/ecl1/extensionpoint/collector/util/JavaProjectContentRetriever.java
@@ -0,0 +1,53 @@
+package net.sf.ecl1.extensionpoint.collector.util;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IPackageFragment;
+import org.eclipse.jdt.core.IPackageFragmentRoot;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.JavaModelException;
+
+/**
+ * Utility class to retrieve certain parts out of a certain project
+ * 
+ * @author keunecke
+ */
+public class JavaProjectContentRetriever {
+
+    private final IJavaProject project;
+
+    public JavaProjectContentRetriever(IJavaProject project) {
+        super();
+        this.project = project;
+    }
+
+    /**
+     * Get all classes out of a java project
+     * @return
+     * @throws JavaModelException
+     */
+    public Collection<IType> getClasses() throws JavaModelException {
+        Collection<IType> result = new ArrayList<IType>();
+        IPackageFragment[] fragmentRoots = project.getPackageFragments();
+        for (IPackageFragment iPackageFragment : fragmentRoots) {
+            if (IPackageFragmentRoot.K_SOURCE == iPackageFragment.getKind()) {
+                scanPackage(iPackageFragment, result);
+            }
+        }
+        return result;
+    }
+
+    private void scanPackage(IPackageFragment iPackageFragment, Collection<IType> result) throws JavaModelException {
+        ICompilationUnit[] units = iPackageFragment.getCompilationUnits();
+        for (ICompilationUnit unit : units) {
+            IType[] types = unit.getAllTypes();
+            for (IType type : types) {
+                result.add(type);
+            }
+        }
+    }
+
+}
