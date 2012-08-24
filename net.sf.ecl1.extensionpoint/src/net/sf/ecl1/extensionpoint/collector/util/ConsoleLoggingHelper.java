58971ee61c8553a8d7527e0a537e2c5e3c8d194a
Markus Keunecke <markuskeunecke@users.sourceforge.net>
2012-08-24 15:44:28 +0200

    start implementing a console logging mechanism

---
+++ net.sf.ecl1.extensionpoint/src/net/sf/ecl1/extensionpoint/collector/util/ConsoleLoggingHelper.java
@@ -0,0 +1,49 @@
+package net.sf.ecl1.extensionpoint.collector.util;
+
+import java.io.IOException;
+
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.ui.console.ConsolePlugin;
+import org.eclipse.ui.console.IConsole;
+import org.eclipse.ui.console.IConsoleManager;
+import org.eclipse.ui.console.MessageConsole;
+import org.eclipse.ui.console.MessageConsoleStream;
+
+public class ConsoleLoggingHelper {
+
+    /** project in which context we are logging */
+    private IJavaProject javaProject;
+
+    private String name;
+
+    public ConsoleLoggingHelper(IJavaProject javaProject, String name) {
+        this.javaProject = javaProject;
+        this.name = name;
+    }
+
+    public void logToConsole(String message) throws IOException {
+        MessageConsole console = findConsole(this.name);
+        MessageConsoleStream newMessageStream = console.newMessageStream();
+        try {
+            newMessageStream.write("[" + this.javaProject.getElementName() + "] " + message);
+            newMessageStream.flush();
+        } finally {
+            newMessageStream.close();
+        }
+
+    }
+
+    private MessageConsole findConsole(String name) {
+        ConsolePlugin plugin = ConsolePlugin.getDefault();
+        IConsoleManager conMan = plugin.getConsoleManager();
+        IConsole[] existing = conMan.getConsoles();
+        for (int i = 0; i < existing.length; i++)
+            if (name.equals(existing[i].getName())) return (MessageConsole) existing[i];
+        //no console found, so create a new one
+        MessageConsole myConsole = new MessageConsole(name, null);
+        conMan.addConsoles(new IConsole[] { myConsole });
+        return myConsole;
+    }
+
+
+}
