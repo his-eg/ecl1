7e3ebdb096f0c75c50b0c50ad8cc376b06cd5668
Markus Keunecke <markuskeunecke@users.sourceforge.net>
2013-06-24 15:52:12 +0200

    implement creation of git repository

---
+++ h1modules.resource.setup/src/h1modules/resource/setup/step/GitInitSetupStep.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package h1modules.resource.setup.step;
+
+import java.io.File;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.InitCommand;
+
+import de.his.cs.sys.extensions.steps.ProjectSetupStep;
+import de.his.cs.sys.extensions.wizards.utils.InitialProjectConfigurationChoices;
+
+/**
+ * Initialize a git repository in the project root
+ * 
+ * @author keunecke
+ *
+ */
+public class GitInitSetupStep implements ProjectSetupStep {
+
+    /* (non-Javadoc)
+     * @see de.his.cs.sys.extensions.steps.ProjectSetupStep#performStep(org.eclipse.core.resources.IProject, de.his.cs.sys.extensions.wizards.utils.InitialProjectConfigurationChoices)
+     */
+    @Override
+    public void performStep(IProject project, InitialProjectConfigurationChoices choices) {
+        InitCommand initCommand = Git.init();
+        IPath rawLocation = project.getFullPath();
+        File ws = ResourcesPlugin.getWorkspace().getRoot().getLocation().toFile();
+        initCommand.setDirectory(new File(ws, rawLocation.lastSegment()));
+        initCommand.call();
+    }
+
+}
