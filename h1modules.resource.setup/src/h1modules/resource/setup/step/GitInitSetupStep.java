ba1b38ab806214d781977a8662392467689f3469
Markus Keunecke <markuskeunecke@users.sourceforge.net>
2014-07-24 21:46:24 +0200

    catch exception

--- h1modules.resource.setup/src/h1modules/resource/setup/step/GitInitSetupStep.java
+++ h1modules.resource.setup/src/h1modules/resource/setup/step/GitInitSetupStep.java
@@ -7,6 +7,7 @@ import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.InitCommand;
+import org.eclipse.jgit.api.errors.GitAPIException;
 
 import de.his.cs.sys.extensions.steps.ProjectSetupStep;
 import de.his.cs.sys.extensions.wizards.utils.InitialProjectConfigurationChoices;
@@ -20,11 +21,17 @@ public class GitInitSetupStep implements ProjectSetupStep {
 
     @Override
     public void performStep(IProject project, InitialProjectConfigurationChoices choices) {
-        InitCommand initCommand = Git.init();
-        IPath rawLocation = project.getFullPath();
-        File ws = ResourcesPlugin.getWorkspace().getRoot().getLocation().toFile();
-        initCommand.setDirectory(new File(ws, rawLocation.lastSegment()));
-        initCommand.call();
+        try {
+        	InitCommand initCommand = Git.init();
+        	IPath rawLocation = project.getFullPath();
+        	File ws = ResourcesPlugin.getWorkspace().getRoot().getLocation().toFile();
+        	initCommand.setDirectory(new File(ws, rawLocation.lastSegment()));
+			initCommand.call();
+		} catch (GitAPIException e) {
+			System.out.println("init of git repo failed");
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
     }
 
 }
