85eed98b8842aafb72c905d7ffb2853559a16f5f
Markus Keunecke <markuskeunecke@users.sourceforge.net>
2014-08-09 12:20:22 +0200

    organize imports

--- h1modules.utilities/src/de/his/cs/sys/extensions/wizards/utils/ResourceSupport.java
+++ h1modules.utilities/src/de/his/cs/sys/extensions/wizards/utils/ResourceSupport.java
@@ -1,8 +1,6 @@
 package de.his.cs.sys.extensions.wizards.utils;
 
 import java.io.ByteArrayInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.util.Collection;
