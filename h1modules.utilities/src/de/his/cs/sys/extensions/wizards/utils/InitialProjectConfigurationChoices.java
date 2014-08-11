c638f5de1c4c2d9a5a636e4b6419fdb5260d666e
Markus Keunecke <markuskeunecke@users.sourceforge.net>
2014-08-11 11:49:07 +0200

    javadoc

--- h1modules.utilities/src/de/his/cs/sys/extensions/wizards/utils/InitialProjectConfigurationChoices.java
+++ h1modules.utilities/src/de/his/cs/sys/extensions/wizards/utils/InitialProjectConfigurationChoices.java
@@ -13,53 +13,55 @@ import java.util.Collection;
 
 /**
  * Container for the initial values for a new extension project entered by a user
- * 
+ *
  * @author keunecke
- * @version $Revision$ 
+ * @version $Revision$
  */
 public class InitialProjectConfigurationChoices {
-	
-	private final Collection<String> projectsToReference;
-	
-	private final String name;
-	
-	private final String version;
 
-	/**
-	 * @param projectsToReference
-	 * @param name
-	 * @param version
-	 */
-	public InitialProjectConfigurationChoices(Collection<String> projectsToReference, String name,
-			String version) {
-		this.projectsToReference = projectsToReference;
-		this.name = name;
-		this.version = version;
-	}
+    private final Collection<String> projectsToReference;
+
+    private final String name;
+
+    private final String version;
+
+    /**
+     * Standard constructor
+     *
+     * @param projectsToReference
+     * @param name
+     * @param version
+     */
+    public InitialProjectConfigurationChoices(Collection<String> projectsToReference, String name,
+                                              String version) {
+        this.projectsToReference = projectsToReference;
+        this.name = name;
+        this.version = version;
+    }
 
     /**
      * Get the projects that will be referenced by a new project
      * @return a collection of project names
      */
-	public Collection<String> getProjectsToReference() {
-		return projectsToReference;
-	}
+    public Collection<String> getProjectsToReference() {
+        return projectsToReference;
+    }
 
     /**
      * The name of the new project
      * @return the name
      */
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
     /**
      * The initial version for the extension
-     * 
+     *
      * @return the version
      */
-	public String getVersion() {
-		return version;
-	}
+    public String getVersion() {
+        return version;
+    }
 
 }
