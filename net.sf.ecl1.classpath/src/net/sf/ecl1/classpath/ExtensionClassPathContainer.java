18c420a04401998eb82038b756edf0d16073f561
Markus Keunecke <markuskeunecke@users.sourceforge.net>
2014-10-19 08:41:52 +0200

    remove unnecessary parameter

--- net.sf.ecl1.classpath/src/net/sf/ecl1/classpath/ExtensionClassPathContainer.java
+++ net.sf.ecl1.classpath/src/net/sf/ecl1/classpath/ExtensionClassPathContainer.java
@@ -25,144 +25,143 @@ import org.eclipse.jdt.core.JavaCore;
 import com.google.common.base.Splitter;
 /**
  * Classpath Container for working with HISinOne-Extensions
- * 
+ *
  * @author markus
  */
 class ExtensionClassPathContainer implements
-		IClasspathContainer {
-	
-	public static final String NET_SF_ECL1_ECL1_CONTAINER_ID = "net.sf.ecl1.ECL1_CONTAINER";
+IClasspathContainer {
 
-	public static final int K_EXTENSION = 99;
-	
-	public static final String EXTENSIONS_FOLDER = "qisserver/WEB-INF/extensions/";
-	
-	private final IJavaProject javaProject;
-	
-	private final Collection<String> extensionsForClassPath = new HashSet<>();
+    public static final String NET_SF_ECL1_ECL1_CONTAINER_ID = "net.sf.ecl1.ECL1_CONTAINER";
 
-	/**
-	 * @param javaProject
-	 * @param containerPath 
-	 */
-	public ExtensionClassPathContainer(IJavaProject javaProject, IPath containerPath) {
-		this.javaProject = javaProject;
-		this.initializeExtensionsToIgnore(containerPath);
-	}
+    public static final int K_EXTENSION = 99;
 
-	/**
-	 * Initiali
-	 * @param containerPath
-	 */
-	private void initializeExtensionsToIgnore(IPath containerPath) {
-		if(containerPath != null && containerPath.segmentCount() == 2) {
-			String commaSeparatedExtensionsToIgnore = containerPath.segment(1);
-			if(commaSeparatedExtensionsToIgnore != null) {
-				List<String> extensionsToIgnore = Splitter.on(",").splitToList(commaSeparatedExtensionsToIgnore);
-				this.extensionsForClassPath.addAll(extensionsToIgnore);
-			}
-		}
-	}
+    public static final String EXTENSIONS_FOLDER = "qisserver/WEB-INF/extensions/";
 
-	@Override
-	public IPath getPath() {
-		return new Path(NET_SF_ECL1_ECL1_CONTAINER_ID);
-	}
+    private final IJavaProject javaProject;
 
-	@Override
-	public int getKind() {
-		return IClasspathEntry.CPE_CONTAINER;
-	}
+    private final Collection<String> extensionsForClassPath = new HashSet<>();
 
-	@Override
-	public String getDescription() {
-		return "ecl1 Extensions Classpath Container";
-	}
+    /**
+     * @param javaProject
+     * @param containerPath
+     */
+    public ExtensionClassPathContainer(IJavaProject javaProject, IPath containerPath) {
+        this.javaProject = javaProject;
+        this.initializeExtensionsToIgnore(containerPath);
+    }
 
-	@Override
-	public IClasspathEntry[] getClasspathEntries() {
-		Map<String, String> extensions = new HashMap<String, String>();
-		
-		scanForExtensionJars(javaProject, extensions);
-		scanForExtensionProjects(extensions);
-		
-		ArrayList<IClasspathEntry> result = new ArrayList<>();
-		
-		//uniquely register extensions either as jar or as project
-		for (Map.Entry<String, String> extension : extensions.entrySet()) {
-			
-			String extensionName = extension.getKey();
-			String extensionPath = extension.getValue();
-		
-			boolean extensionNeedsToBeExported = extensionNeedsToBeAddedToClasspath(extensionName);
-			if(extensionNeedsToBeExported) {
-				if(extensionPath.endsWith(".jar")) {
-					IPath path = javaProject.getPath().append(EXTENSIONS_FOLDER).append(extensionPath);
-					//create a lib entry
-					System.out.println("Creating new container entry for: " + path.toString() 
-							+ " exported: " + extensionNeedsToBeExported);
-					IClasspathEntry libraryEntry = JavaCore.newLibraryEntry(path, path, path, true);
-					result.add(libraryEntry);
-				} else {
-					IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(extensionPath);
-					System.out.println("Creating new container entry for project: " + project.getName() 
-							+ " exported: " + extensionNeedsToBeExported);
-					JavaCore.newProjectEntry(project.getLocation(), true);
-				}
-			}
-		}
-		return result.toArray(new IClasspathEntry[1]);
-	}
+    /**
+     * Initiali
+     * @param containerPath
+     */
+    private void initializeExtensionsToIgnore(IPath containerPath) {
+        if(containerPath != null && containerPath.segmentCount() == 2) {
+            String commaSeparatedExtensionsToIgnore = containerPath.segment(1);
+            if(commaSeparatedExtensionsToIgnore != null) {
+                List<String> extensionsToIgnore = Splitter.on(",").splitToList(commaSeparatedExtensionsToIgnore);
+                this.extensionsForClassPath.addAll(extensionsToIgnore);
+            }
+        }
+    }
 
-	private boolean extensionNeedsToBeAddedToClasspath(String extensionName) {
-		boolean configuredExtensionsEmpty = extensionsForClassPath.isEmpty();
-		boolean extensionIsListed = extensionsForClassPath.contains(extensionName);
-		return configuredExtensionsEmpty || extensionIsListed;
-	}
-	
-	/**
-	 * 
-	 * @param javaProject
-	 * @param extensions
-	 */
-	private void scanForExtensionProjects(Map<String, String> extensions) {
-		//scan workspace for extension projects
-		IWorkspaceRoot ws = ResourcesPlugin.getWorkspace().getRoot();
-		List<IProject> projects = Arrays.asList(ws.getProjects(0));
-		for (IProject project : projects) {
-			if(isExtensionProject(project)) {
-				extensions.put(project.getName(), project.getName());
-			}
-		}
-	}
-	
-	private boolean isExtensionProject(IProject project) {
-		IFile file = project.getFile("extension.ant.properties");
-		return file.exists();
-	}
+    @Override
+    public IPath getPath() {
+        return new Path(NET_SF_ECL1_ECL1_CONTAINER_ID);
+    }
 
-	/**
-	 * Scan the java project for jar files in the extensions folder
-	 * 
-	 * @param javaProject
-	 * @param extensions
-	 */
-	private void scanForExtensionJars(IJavaProject javaProject,
-			Map<String, String> extensions) {
-		//scan workspace for extension jars
-		IFolder extensionsFolder = javaProject.getProject().getFolder(EXTENSIONS_FOLDER);
-		if(extensionsFolder.exists()) {
-			//if there is an extensions folder, scan it
-			IPath rawLocation = extensionsFolder.getRawLocation();
-			List<File> extensionJars = Arrays.asList(rawLocation.toFile().listFiles(new FilenameFilter() {
-				@Override
-				public boolean accept(File dir, String name) {
-					return name != null && name.endsWith("jar");
-				}
-			}));
-			for (File extensionJar : extensionJars) {
-				extensions.put(extensionJar.getName().replace(".jar", ""), extensionJar.getName());
-			}
-		}
-	}
+    @Override
+    public int getKind() {
+        return IClasspathEntry.CPE_CONTAINER;
+    }
+
+    @Override
+    public String getDescription() {
+        return "ecl1 Extensions Classpath Container";
+    }
+
+    @Override
+    public IClasspathEntry[] getClasspathEntries() {
+        Map<String, String> extensions = new HashMap<String, String>();
+
+        scanForExtensionJars(extensions);
+        scanForExtensionProjects(extensions);
+
+        ArrayList<IClasspathEntry> result = new ArrayList<>();
+
+        //uniquely register extensions either as jar or as project
+        for (Map.Entry<String, String> extension : extensions.entrySet()) {
+
+            String extensionName = extension.getKey();
+            String extensionPath = extension.getValue();
+
+            boolean extensionNeedsToBeExported = extensionNeedsToBeAddedToClasspath(extensionName);
+            if(extensionNeedsToBeExported) {
+                if(extensionPath.endsWith(".jar")) {
+                    IPath path = javaProject.getPath().append(EXTENSIONS_FOLDER).append(extensionPath);
+                    //create a lib entry
+                    System.out.println("Creating new container entry for: " + path.toString()
+                                       + " exported: " + extensionNeedsToBeExported);
+                    IClasspathEntry libraryEntry = JavaCore.newLibraryEntry(path, null, null, true);
+                    result.add(libraryEntry);
+                } else {
+                    IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(extensionPath);
+                    System.out.println("Creating new container entry for project: " + project.getName()
+                                       + " exported: " + extensionNeedsToBeExported);
+                    result.add(JavaCore.newProjectEntry(project.getLocation(), true));
+                }
+            }
+        }
+        return result.toArray(new IClasspathEntry[1]);
+    }
+
+    private boolean extensionNeedsToBeAddedToClasspath(String extensionName) {
+        boolean configuredExtensionsEmpty = extensionsForClassPath.isEmpty();
+        boolean extensionIsListed = extensionsForClassPath.contains(extensionName);
+        return configuredExtensionsEmpty || extensionIsListed;
+    }
+
+    /**
+     *
+     * @param javaProject
+     * @param extensions
+     */
+    private void scanForExtensionProjects(Map<String, String> extensions) {
+        //scan workspace for extension projects
+        IWorkspaceRoot ws = ResourcesPlugin.getWorkspace().getRoot();
+        List<IProject> projects = Arrays.asList(ws.getProjects(0));
+        for (IProject project : projects) {
+            if(isExtensionProject(project)) {
+                extensions.put(project.getName(), project.getName());
+            }
+        }
+    }
+
+    private boolean isExtensionProject(IProject project) {
+        IFile file = project.getFile("extension.ant.properties");
+        return file.exists();
+    }
+
+    /**
+     * Scan the java project for jar files in the extensions folder
+     *
+     * @param javaProject
+     * @param extensions
+     */
+    private void scanForExtensionJars(Map<String, String> extensions) {
+        //scan workspace for extension jars
+        IFolder extensionsFolder = javaProject.getProject().getFolder(EXTENSIONS_FOLDER);
+        if(extensionsFolder.exists()) {
+            //if there is an extensions folder, scan it
+            IPath rawLocation = extensionsFolder.getRawLocation();
+            List<File> extensionJars = Arrays.asList(rawLocation.toFile().listFiles(new FilenameFilter() {
+                @Override
+                public boolean accept(File dir, String name) {
+                    return name != null && name.endsWith("jar");
+                }
+            }));
+            for (File extensionJar : extensionJars) {
+                extensions.put(extensionJar.getName().replace(".jar", ""), extensionJar.getName());
+            }
+        }
+    }
 }
