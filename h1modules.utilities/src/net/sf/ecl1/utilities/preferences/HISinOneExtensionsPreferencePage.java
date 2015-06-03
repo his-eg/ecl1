ef1539adf7e3e62ca247398a74eb037ca90148d2
Markus Keunecke <markuskeunecke@users.sourceforge.net>
2015-06-03 13:51:27 +0200

    improve java doc

--- h1modules.utilities/src/net/sf/ecl1/utilities/preferences/HISinOneExtensionsPreferencePage.java
+++ h1modules.utilities/src/net/sf/ecl1/utilities/preferences/HISinOneExtensionsPreferencePage.java
@@ -8,17 +8,7 @@ import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 
 /**
- * This class represents a preference page that
- * is contributed to the Preferences dialog. By
- * subclassing <samp>FieldEditorPreferencePage</samp>, we
- * can use the field support built into JFace that allows
- * us to create a page that is small and knows how to
- * save, restore and apply itself.
- * <p>
- * This page is used to modify preferences only. They
- * are stored in the preference store that belongs to
- * the main plug-in class. That way, preferences can
- * be accessed directly via the preference store.
+ * HISinOne-Extension-Tools preferences page
  */
 
 public class HISinOneExtensionsPreferencePage
@@ -49,6 +39,7 @@ implements IWorkbenchPreferencePage {
      */
     @Override
     public void init(IWorkbench workbench) {
+        //nop
     }
 
 }
--- h1modules.utilities/src/net/sf/ecl1/utilities/preferences/PreferenceConstants.java
+++ h1modules.utilities/src/net/sf/ecl1/utilities/preferences/PreferenceConstants.java
@@ -1,14 +1,23 @@
 package net.sf.ecl1.utilities.preferences;
 
 /**
- * Constant definitions for plug-in preferences
+ * Constant definitions for HISinOne-Extension tools plug-in preferences
  */
 public class PreferenceConstants {
 
+    /**
+     * Preference for URI to git server
+     */
     public static final String GIT_SERVER_PREFERENCE = "gitServer";
 
+    /**
+     * Preference for URL of jenkins build server
+     */
     public static final String BUILD_SERVER_PREFERENCE = "buildServer";
 
+    /**
+     * Preference for search view on jenkins build server
+     */
     public static final String BUILD_SERVER_VIEW_PREFERENCE = "buildServerView";
 
 }
