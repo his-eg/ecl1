d0897afea49e7386b015af670d7bd2ee76de0df2
Markus Keunecke <markuskeunecke@users.sourceforge.net>
2015-06-10 09:26:25 +0200

    recognize default ports

--- h1modules.utilities/src/net/sf/ecl1/utilities/preferences/NetUtil.java
+++ h1modules.utilities/src/net/sf/ecl1/utilities/preferences/NetUtil.java
@@ -3,6 +3,9 @@ package net.sf.ecl1.utilities.preferences;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.URL;
+import java.util.Map;
+
+import com.google.common.collect.Maps;
 
 /**
  * Verify a text control by trying to open a socket
@@ -11,6 +14,14 @@ import java.net.URL;
  *
  */
 public class NetUtil {
+
+    private static final Map<String, Integer> defaultPorts;
+    static {
+        defaultPorts = Maps.newHashMap();
+        defaultPorts.put("ssh", Integer.valueOf(22));
+        defaultPorts.put("http", Integer.valueOf(80));
+        defaultPorts.put("https", Integer.valueOf(443));
+    }
 
     /**
      * determine if socket can be opened
@@ -24,8 +35,15 @@ public class NetUtil {
         try {
             //exchange protocol from ssh to http as ssh is not a supported protocol
             String urlToCheck = serverUrl.replace("ssh://", "http://");
+            int positionOfProtocollDelimiter = serverUrl.indexOf("://");
+            String protocoll = serverUrl.substring(0, positionOfProtocollDelimiter);
+
             URL url = new URL(urlToCheck);
-            socket = new Socket(url.getHost(), url.getPort());
+            int port = url.getPort();
+            if (port <= 0) {
+                port = defaultPorts.get(protocoll).intValue();
+            }
+            socket = new Socket(url.getHost(), port);
         } catch (IOException e) {
             return false;
         }
