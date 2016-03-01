b161af9098231591b0cbb2754d937a15550b3a32
Markus Keunecke <markuskeunecke@users.sourceforge.net>
2016-03-01 13:33:14 +0100

    add a release script to automate every step after version bumping and update site building

---
+++ release.sh
@@ -0,0 +1,46 @@
+###############################################################################
+#									      
+# Release script for ecl1
+#
+# Prerequisites: 
+# - Prebuilt Update Site
+# - build.ant-private.properties, 
+# - README.md up to date
+#
+# Parameters: 1. Version identificator (required)                          
+#									      
+###############################################################################
+VERSION=$1
+###############################################################################
+
+# Sign update site artifacts
+cd h1updatesite
+ant sign
+git commit -am "Signed update site artifacts for version ${VERSION}"
+
+cd ..
+
+# Website
+cd net.sf.ecl1.website
+
+# Update version number
+cat index.html.template | sed "s|ECL1VERSION|$VERSION|" > index.html.tmp
+mv index.html.tmp index.html
+cd ..
+
+# Tag version
+git tag -a v$VERSION -m "Release tag for version ${VERSION}"
+
+# Upload website
+cd net.sf.ecl1.website
+./update.sh
+cd ..
+
+# push tags to sf.net
+git push --tags
+git push
+
+# upload update site and update site archive
+cd h1updatesite
+ant upload
+cd ..
