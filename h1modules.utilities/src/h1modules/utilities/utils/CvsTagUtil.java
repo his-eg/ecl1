0878d96786b7d6007e8651691888709704efab69
Tilman Neumann <tneumann@his.de>
2017-04-26 12:18:25 +0200

    documented new webapps branch name convention +++ removed obsolete method

--- h1modules.utilities/src/h1modules/utilities/utils/CvsTagUtil.java
+++ h1modules.utilities/src/h1modules/utilities/utils/CvsTagUtil.java
@@ -1,8 +1,5 @@
 package h1modules.utilities.utils;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 
@@ -25,8 +22,9 @@ public class CvsTagUtil {
      * @return webapps project branch version in long notation, read from webapps/CVS/Tag.</br>
      * 
      * Examples:</br>
-     * HISinOne_VERSION_07</br>
-     * HISinOne_VERSION_06_RELEASE_01
+     * RELEASE_2017_06                (new convention)</br>
+     * HISinOne_VERSION_07            (old convention)</br>
+     * HISinOne_VERSION_06_RELEASE_01 (old convention)
      */
     public static String getCvsTagVersionLongString() {
         IProject webapps = HISinOneFileUtil.getWebapps();
@@ -43,40 +41,5 @@ public class CvsTagUtil {
             }
         }
         return UNKNOWN_VERSION;
-    }
-    
-    /**
-     * @return webapps project branch and respective version number in short notation, read from webapps/CVS/Tag.</br>
-     *
-     * Examples:</br>
-     * old convention:
-     * HISinOne_VERSION_07 --> 7.0</br>
-     * HISinOne_VERSION_06_RELEASE_01 --> 6.1
-     * new convention:
-     * HISinOne_2017_06 -> 2017.6
-     */
-    public static String getCvsTagVersionShortString() {
-        String longVersion = getCvsTagVersionLongString();
-        if (longVersion.equals(HEAD_VERSION) || longVersion.equals(UNKNOWN_VERSION)) {
-        	return longVersion;
-        }
-
-        // extract short version from old long version format
-        String reducedBranch = longVersion.trim().replace(HISINONE_PREFIX, "");
-        List<String> branchNameComponents = Arrays.asList(reducedBranch.split("_"));
-        if (reducedBranch.startsWith(VERSION_PREFIX)) {
-        	// old convention like HISinOne_VERSION_07 or HISinOne_VERSION_06_RELEASE_01
-	        String majorVersion = Integer.toString(Integer.parseInt(branchNameComponents.get(1)));
-	        String minorVersion = "0";
-	        if (branchNameComponents.size() > 2) {
-	            minorVersion = Integer.toString(Integer.parseInt(branchNameComponents.get(3)));
-	        }
-	        return majorVersion + "." + minorVersion;
-        } else {
-        	// new convention like HISinOne_2017_06
-        	String majorVersion = Integer.toString(Integer.parseInt(branchNameComponents.get(0)));
-        	String minorVersion = Integer.toString(Integer.parseInt(branchNameComponents.get(1)));
-	        return majorVersion + "." + minorVersion;
-        }
     }
 }
