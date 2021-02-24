f830ecd17cffba477a3d89df4b04b307a6ad5d19
Simon Sohrt <sohrt@his.de>
2021-02-24 10:49:24 +0100

    #252175: Fixed a bug: If there was a space within the path to either eclipse or the ecl1-plugin, the updatehooks-Extensions failed to locate the commit-msg-hook-script within the ecl1-plugin and crashed. This happend both on Linux and on Windows.

--- net.sf.ecl1.git.updatehooks/src/net/sf/ecl1/git/updatehooks/UpdateHooks.java
+++ net.sf.ecl1.git.updatehooks/src/net/sf/ecl1/git/updatehooks/UpdateHooks.java
@@ -1,37 +1,31 @@
 package net.sf.ecl1.git.updatehooks;
 
-import java.io.File;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
+import java.io.InputStreamReader;
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import java.util.Map;
 import java.util.Map.Entry;
 import java.util.TreeMap;
 
-import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.jgit.transport.resolver.FileResolver;
 import org.eclipse.jgit.util.FS;
 import org.eclipse.jgit.util.FS_Win32;
-import org.eclipse.jgit.util.FS_Win32_Cygwin;
 import org.eclipse.ui.IStartup;
-import org.osgi.framework.Bundle;
-
-import com.sun.org.apache.xpath.internal.ExtensionsProvider;
-
 import net.sf.ecl1.utilities.general.ConsoleLogger;
 import net.sf.ecl1.utilities.hisinone.ExtensionUtil;
 import net.sf.ecl1.utilities.hisinone.WebappsUtil;
@@ -42,7 +36,7 @@ public class UpdateHooks implements IStartup {
     
     private static final String HOOKS_DIR_ECLIPSE_PROJECTS = ".git/hooks/commit-msg";
     private static final String HOOKS_DIR_ECL1 = "/githooks/commit-msg";
-    private static final int MAX_SIZE_OF_HOOK = 50000;
+    
 	
     
 	@Override
@@ -82,9 +76,19 @@ public class UpdateHooks implements IStartup {
 					 * http://blog.vogella.com/2010/07/06/reading-resources-from-plugin/
 					 */
 					URL url = new URL("platform:/plugin/" + UpdateHooksActivator.PLUGIN_ID + HOOKS_DIR_ECL1);
-					hooksDirEcl1 = Paths.get(FileLocator.resolve(url).toURI());
+
+					URL fileURL = FileLocator.resolve(url);
 					
-				} catch (URISyntaxException | IOException e) {
+					/*
+					 * Convert URL to URI. We need to correctly handle spaces within the URL. 
+					 * (Spaces are created, for example, by .../Program Files/... on Windows machines)
+					 */		
+					URI uri = new URI(fileURL.toString().replace(" ", "%20"));
+										
+					//Convert URI to Path
+					hooksDirEcl1 = Paths.get(uri);
+					
+				} catch (IOException | URISyntaxException e) {
 					logger.error2("Failed to locate the git hooks within the ecl1 plugin. Exception: " + e.getMessage(), e);
 					return Status.CANCEL_STATUS;
 				}
