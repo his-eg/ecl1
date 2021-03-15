aa83b9208a116673560fe44fecde5bc7ce76de23
Simon Sohrt <sohrt@his.de>
2021-03-15 10:33:40 +0100

    Two changes to auto-lfs-prune: 1. prune can now be canceled by the user 2. Changed the way how the task is scheduled repeatedly

--- net.sf.ecl1.git.auto-lfs-prune/src/net/sf/ecl1/git/auto/lfs/prune/AutoLfsPrune.java
+++ net.sf.ecl1.git.auto-lfs-prune/src/net/sf/ecl1/git/auto/lfs/prune/AutoLfsPrune.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -22,6 +23,7 @@ public class AutoLfsPrune implements IStartup {
     
 	private static final ConsoleLogger logger = new ConsoleLogger(AutoLfsPruneActivator.getDefault().getLog(), AutoLfsPruneActivator.PLUGIN_ID, AutoLfsPrune.class.getSimpleName());
 	
+//	private static int RUN_DELAY = 60000; // =1min
 	private static int RUN_DELAY = 86400000; //86400000ms = 1day
     
 	public static ExecutionResult runCommandInRepo(String command, Repository repo) throws IOException, InterruptedException {
@@ -31,14 +33,14 @@ public class AutoLfsPrune implements IStartup {
 		ProcessBuilder pb = fs.runInShell(command, new String[0]);
 		pb.directory(repo.getWorkTree());
 		pb.environment().put(Constants.GIT_DIR_KEY, repo.getDirectory().getAbsolutePath());
-		
+	
 		return fs.execute(pb, null);
 				
 	}
     
     @Override
 	public void earlyStartup() {
-		Job job = new Job("ecl1GitLfsPrune") {
+		Job job = new Job("ecl1GitLfsPrune") {			
 			@Override
 			protected IStatus run(IProgressMonitor monitor) {
 				
@@ -47,6 +49,14 @@ public class AutoLfsPrune implements IStartup {
 				monitor.beginTask("Batch Git Lfs prune", projects.size());
 				
 				for (IProject p : projects) {
+					/*
+					 * Check if the user has requested canceling of pruning
+					 */
+					if(monitor.isCanceled()) {
+						return Status.CANCEL_STATUS;
+					}
+					
+					
 					String name = p.getName();
 					monitor.subTask("Pruning " + name);
 					File projectLocationFile = p.getLocation().append(".git").toFile();
@@ -86,7 +96,7 @@ public class AutoLfsPrune implements IStartup {
 						if (er.getStdout().length() != 0) {
 							// If "git stash list" has output --> stashes present in this repo --> We cannot prune
 							logger.info("Found stashes in project: " + name + ". Cannot run \"git lfs prune\" when stashes are present! Aborting..."
-											+ "\nYou can run \"git stash clear\" to manually delete your stashes. ");
+									+ "\nYou can run \"git stash clear\" to manually delete your stashes. ");
 							monitor.worked(1);
 							continue;
 						}
@@ -126,11 +136,44 @@ public class AutoLfsPrune implements IStartup {
 				}
 				
 				monitor.done();
-				//Rerung "git lfs prune" once every day
-				this.schedule(RUN_DELAY);
 				return Status.OK_STATUS;
 			}
 		};
 		job.schedule();
+		
+		
+
+		/*
+		 * What does this do?
+		 * Reschedule "git lfs prune" once a day. 
+		 * 
+		 * Why is this necessary?
+		 * At least one user is leaving eclipse open for a very long time (multiple days). This user
+		 * wants eclipse to run "git lfs prune" once a day automatically. 
+		 * 
+		 * This thread will never terminated. Isn't there a better way to schedule an indefinitely repeating job?
+		 * The officially recommended way of implementing a repeating background task is described here: 
+		 * https://wiki.eclipse.org/FAQ_How_do_I_create_a_repeating_background_task%3F
+		 * Using this approach would also keep the thread alive indefinitely. Therefore there is no 
+		 * difference resource-wise between the recommended approach and the approach we have chosen here. 
+		 *  
+		 * The recommended approach has one downside, however: 
+		 * The progressbar for this job will not disappear and will instead indicate a sleeping thread to the user.  
+		 * This is correct (since the thread is indeed sleeping), but some users (including me) did not like the indication
+		 * of a sleeping thread that never goes away (actually I discovered, that it is no longer displayed, when closing and 
+		 * re-opening the progress-view, but some users never close the progress-view and therefore the sleeping indication never goes away).
+		 * By finishing the old job, the progress bar is completely discarded and no sleeping thread is indicated to the user.  
+		 */
+		while(true) {
+			try {
+				Thread.sleep(RUN_DELAY);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			job.schedule();
+		}
+			
+
 	}
 }
