5f38a275ab258249ccbecb4f49da5504bfa21e06
Simon Sohrt <sohrt@his.de>
2022-01-27 15:25:11 +0100

    #269975: While running the extension creation wizard supresses builds. This ensures that the wizard does not freeze while being executed

--- h1modules/src/de/his/cs/sys/extensions/Activator.java
+++ h1modules/src/de/his/cs/sys/extensions/Activator.java
@@ -1,5 +1,6 @@
 package de.his.cs.sys.extensions;
 
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
@@ -13,6 +14,14 @@ public class Activator extends AbstractUIPlugin {
 
 	/** The shared instance */
 	private static Activator plugin;
+	
+	private Job job;
+	
+	
+
+	public void setJob(Job job) {
+		this.job = job;
+	}
 
 	/**
 	 * starts the plugin
@@ -22,12 +31,17 @@ public class Activator extends AbstractUIPlugin {
 	 */
 	@Override
 	public void start(BundleContext context) throws Exception {
+		if (job != null) {
+			job.cancel();
+			job.join();
+		}
 		super.start(context);
 		plugin = this;
 	}
 
 	@Override
 	public void stop(BundleContext context) throws Exception {
+		
 		plugin = null;
 		super.stop(context);
 	}
--- h1modules/src/de/his/cs/sys/extensions/wizards/NewExtensionProjectWizard.java
+++ h1modules/src/de/his/cs/sys/extensions/wizards/NewExtensionProjectWizard.java
@@ -4,11 +4,18 @@ package de.his.cs.sys.extensions.wizards;
 import java.net.URI;
 
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.WorkspaceJob;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
 
+import de.his.cs.sys.extensions.Activator;
 import de.his.cs.sys.extensions.extensionpointhandlers.ForEachProjectSetupStepHandler;
 import de.his.cs.sys.extensions.wizards.pages.NewExtensionWizardPage;
 import net.sf.ecl1.utilities.general.InitialProjectConfigurationChoices;
@@ -37,13 +44,36 @@ public class NewExtensionProjectWizard extends Wizard implements INewWizard {
 
 	@Override
 	public boolean performFinish() {
-		URI location = null;
+		final URI location;
 		if(!firstPage.useDefaults()) {
 			location = firstPage.getLocationURI();
-		}
+		} else {
+			location = null; 
+		}	
 		InitialProjectConfigurationChoices initialChoice = firstPage.getInitialConfiguration();
-		IProject project = new ProjectSupport(firstPage.getStrategy().packagesToCreate(initialChoice.getName())).createProject(initialChoice, location);
-		new ForEachProjectSetupStepHandler(project, initialChoice).contribute();
+		
+		/*
+		 * We are wrapping the actual creation of the extension project into a workspace job, because
+		 * workspace jobs are never interrupted by the eclipse build process (since they are considered somewhat atomic). 
+		 * 
+		 * This prevents the wizard dialog from freezing in case that a build job has begun between starting 
+		 * the wizard and performing the finish. 
+		 */
+		Job createExtensionProjectJob = new WorkspaceJob("ecl1: Creating a new extension project") {
+			
+			@Override
+			public IStatus runInWorkspace(IProgressMonitor monitor) throws CoreException {
+				IProject project = new ProjectSupport(firstPage.getStrategy().packagesToCreate(initialChoice.getName())).createProject(initialChoice, location);
+				new ForEachProjectSetupStepHandler(project, initialChoice).contribute();
+				//Exit job
+				return Status.OK_STATUS;
+			}
+		};
+		/*Register job with activator to shutdown the job properly in case eclipse is closed */
+		Activator.getDefault().setJob(createExtensionProjectJob);
+		createExtensionProjectJob.schedule();
+		
+		//Exit wizard
 		return true;
 	}
 
