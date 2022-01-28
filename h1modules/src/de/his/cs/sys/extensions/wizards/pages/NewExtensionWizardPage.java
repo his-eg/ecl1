71f6d53c339445acc0c39211e6bb5b0a1ff0d307
Simon Sohrt <sohrt@his.de>
2022-01-28 11:26:04 +0100

    #269975: Prettified the design of the NewExtensionWizardPage ;) Also added a warning in case no webapps is found in the workspace.

--- h1modules/src/de/his/cs/sys/extensions/wizards/pages/NewExtensionWizardPage.java
+++ h1modules/src/de/his/cs/sys/extensions/wizards/pages/NewExtensionWizardPage.java
@@ -3,9 +3,12 @@ package de.his.cs.sys.extensions.wizards.pages;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import org.eclipse.core.resources.IProject;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.List;
 import org.eclipse.swt.widgets.Text;
@@ -16,6 +19,7 @@ import de.his.cs.sys.extensions.wizards.pages.packages.PackageStructureStrategy;
 import net.sf.ecl1.utilities.general.InitialProjectConfigurationChoices;
 import net.sf.ecl1.utilities.general.WorkspaceSupport;
 import net.sf.ecl1.utilities.hisinone.HisConstants;
+import net.sf.ecl1.utilities.hisinone.WebappsUtil;
 
 /**
  * Extended New Project Wizard Page asking for additional information on extensions
@@ -43,20 +47,22 @@ public class NewExtensionWizardPage extends WizardNewProjectCreationPage {
 		super.createControl(parent);
 		Composite control = (Composite) getControl();
 		
-		Composite versionChoice = new Composite(control, SWT.LEFT);
-		GridLayout versionGrid = new GridLayout(2, false);
-		versionChoice.setLayout(versionGrid);
-		Label versionInputLabel = new Label(versionChoice, SWT.LEFT);
-		versionInputLabel.setText("Initial Extension Version");
-		versionInputTextField = new Text(versionChoice, SWT.LEFT);
-		versionInputTextField.setText("0.0.1");
-		
-		Composite projectChoice = new Composite(control, SWT.BORDER | SWT.TOP);
+		Composite composite = new Composite(control, SWT.LEFT);
+		//Every component with this griddata grabs excess vertical and horizontal space
+		GridData gridData = new GridData(SWT.FILL, SWT.FILL, true, true); 
 		GridLayout gl = new GridLayout(2, false);
-		projectChoice.setLayout(gl);
-		Label projectChoiceLabel = new Label(projectChoice, SWT.TOP);
+		composite.setLayout(gl);
+		composite.setLayoutData(gridData);
+		Label versionInputLabel = new Label(composite, SWT.LEFT);
+		versionInputLabel.setText("Initial Extension Version");
+		versionInputTextField = new Text(composite, SWT.LEFT );
+		versionInputTextField.setText("0.0.1");
+		versionInputTextField.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false)); //Grab excess horizontal space
+		
+		Label projectChoiceLabel = new Label(composite, SWT.TOP);
 		projectChoiceLabel.setText("Referenced Projects");
-		projectList = new List(projectChoice, SWT.BORDER | SWT.MULTI | SWT.V_SCROLL);
+		projectList = new List(composite, SWT.BORDER | SWT.MULTI | SWT.V_SCROLL);
+		projectList.setLayoutData(gridData); //Grab excess vertical and horizontal space
 		java.util.List<String> references = new WorkspaceSupport().getPossibleProjectsToReference();
 		int index = 0;
 		for (String ref : references) {
@@ -66,6 +72,19 @@ public class NewExtensionWizardPage extends WizardNewProjectCreationPage {
 			}
 		}
 		projectList.select(index);
+		
+		Label warnAboutOldTemplatesLabel = new Label(composite, SWT.TOP);
+		warnAboutOldTemplatesLabel.setLayoutData(new GridData(SWT.LEFT, SWT.TOP, true , false , 2, 1)); //Spans over two columns
+		warnAboutOldTemplatesLabel.setForeground(Display.getDefault().getSystemColor(SWT.COLOR_RED));
+		IProject webapps = WebappsUtil.findWebappsProject();
+		if ( webapps == null ) {
+			warnAboutOldTemplatesLabel.setText("No webapps in workspace detected! The latest templates are located in webapps.\n"
+					+ "Please import webapps into your workspace to use the latest templates.\n"
+					+ "Falling back to templates from ecl1 that are likely outdated.");
+		}
+
+ 
+
 	}
 	
 	/**
