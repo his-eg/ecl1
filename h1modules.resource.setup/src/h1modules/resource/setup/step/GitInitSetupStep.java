40fa5a0a9f37fd0d4890ad88112bd8798a7d7a09
Simon Sohrt <sohrt@his.de>
2022-09-22 14:22:02 +0200

    #286346: Ecl1 will try to connect the newly created extension with the correct git-repo at gitlab.his.de

--- h1modules.resource.setup/src/h1modules/resource/setup/step/GitInitSetupStep.java
+++ h1modules.resource.setup/src/h1modules/resource/setup/step/GitInitSetupStep.java
@@ -1,13 +1,17 @@
 package h1modules.resource.setup.step;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.InitCommand;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.ConfigConstants;
+import org.eclipse.jgit.lib.StoredConfig;
+import org.eclipse.jgit.transport.URIish;
 
 import de.his.cs.sys.extensions.steps.ProjectSetupStep;
 import h1modules.resource.setup.Activator;
@@ -22,17 +26,60 @@ import net.sf.ecl1.utilities.general.InitialProjectConfigurationChoices;
 public class GitInitSetupStep implements ProjectSetupStep {
 
     private static final ConsoleLogger logger = new ConsoleLogger(Activator.getDefault().getLog(), Activator.PLUGIN_ID, GitInitSetupStep.class.getSimpleName());
-
+    
     @Override
     public void performStep(IProject project, InitialProjectConfigurationChoices choices) {
-        try {
-        	InitCommand initCommand = Git.init();
-        	IPath rawLocation = project.getFullPath();
-        	File ws = ResourcesPlugin.getWorkspace().getRoot().getLocation().toFile();
-        	initCommand.setDirectory(new File(ws, rawLocation.lastSegment()));
-			initCommand.call();
-		} catch (GitAPIException e) {
-			logger.error2("Git repository initialization failed: " + e.getMessage(), e);
-		}
+    	String remoteURI = calculateRemoteURI(project);
+    	if (remoteURI == null ) {
+			logger.info("Project name was not created according to the HIS naming convention" +
+					"\nBecause the naming convention was violeted, no git repo will be created and no attempt" +  
+					" will be made to connect to gilab.his.de!" + 
+					"\nNaming convention can be found here: https://wiki.his.de/mediawiki/index.php/DV-Konzept_HISinOne-Extension#Namenskonvention");
+    		return;
+    	}
+    	
+    	IPath rawLocation = project.getFullPath();
+    	File ws = ResourcesPlugin.getWorkspace().getRoot().getLocation().toFile();
+    		
+		try {
+			//Git init
+			Git gitRepo = Git.init()
+					.setDirectory(new File(ws, rawLocation.lastSegment()))
+					.call();
+			
+			//Add remote
+			gitRepo.remoteAdd()
+				.setUri(new URIish(remoteURI))
+				.setName("origin")
+				.call();
+			
+			//Associate local branch with remote branch to configure push/pull
+			//Taken from here: https://stackoverflow.com/a/27825315
+			StoredConfig config = gitRepo.getRepository().getConfig();
+			config.setString(ConfigConstants.CONFIG_BRANCH_SECTION, "master", "remote", "origin");
+			config.setString(ConfigConstants.CONFIG_BRANCH_SECTION, "master" , "merge", "refs/heads/master");
+			config.save();
+			
+			//Pull
+			gitRepo.pull().call();
+			
+			logger.info("Successfully pulled local git repo and associated the repo with the following remote address: " + remoteURI);
+			
+		} catch (GitAPIException | URISyntaxException | IOException  e) {
+			logger.error2("Couldn't setup local git repo."
+				+ "\nTherefore no git repo was created at all. Please setup git repo manually."
+				+ "\nOne possible reason for this could be that the remote git repo does not exist (yet)."
+				+ "\nThis was the exception: " + e.getCause(), e);
+		} 
+    	
     }
+
+	private String calculateRemoteURI(IProject project) {
+		String projectName = project.getName();
+		String[] splittedSegments = projectName.split("\\.");
+		if (splittedSegments.length < 3) {
+			return null;
+		}
+		return "ssh://git@gitlab.his.de/h1/" + splittedSegments[0] + "/" + splittedSegments[1] + "/" + projectName;
+	}
 }
--- h1modules/src/de/his/cs/sys/extensions/wizards/NewExtensionProjectWizard.java
+++ h1modules/src/de/his/cs/sys/extensions/wizards/NewExtensionProjectWizard.java
@@ -4,6 +4,7 @@ package de.his.cs.sys.extensions.wizards;
 import java.net.URI;
 
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.WorkspaceJob;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -65,6 +66,15 @@ public class NewExtensionProjectWizard extends Wizard implements INewWizard {
 			public IStatus runInWorkspace(IProgressMonitor monitor) throws CoreException {
 				IProject project = new ProjectSupport(firstPage.getStrategy().packagesToCreate(initialChoice.getName())).createProject(initialChoice, location);
 				new ForEachProjectSetupStepHandler(project, initialChoice).contribute();
+				
+				//Close and open project to fully "refresh" the project (by doing this
+				//eclipse realizes that this project is a git project)
+				project.close(monitor);
+				//Refresh is necessary, because otherwise eclipse might not register that 
+				//the project was closed.
+				project.refreshLocal(IResource.DEPTH_INFINITE, monitor);
+				project.open(monitor);
+				
 				//Exit job
 				return Status.OK_STATUS;
 			}
