abbbe739bdfd27b7b9c5ca9f8f188628623730fb
Lukas Hitzemann <45481788+Batschko@users.noreply.github.com>
2025-03-14 16:31:54 +0100

    #336831: Add App class to run UpdateHooks

---
+++ net.sf.ecl1.git.updatehooks/src/net/sf/ecl1/git/updatehooks/standalone/UpdateHooksApp.java
@@ -0,0 +1,10 @@
+package net.sf.ecl1.git.updatehooks.standalone;
+
+import net.sf.ecl1.git.updatehooks.UpdateHooks;
+
+public class UpdateHooksApp {
+
+    public static void main(String[] args) {
+        new UpdateHooks().earlyStartup();
+    }
+}
