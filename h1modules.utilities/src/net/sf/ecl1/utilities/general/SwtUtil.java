cd99c2688f1b16e2ab6bbf1818af103a15216eef
Lukas Hitzemann <45481788+Batschko@users.noreply.github.com>
2025-03-17 16:20:22 +0100

    #336831: Add class SwtUtil

---
+++ h1modules.utilities/src/net/sf/ecl1/utilities/general/SwtUtil.java
@@ -0,0 +1,34 @@
+package net.sf.ecl1.utilities.general;
+
+import org.eclipse.swt.widgets.Display;
+
+import net.sf.ecl1.utilities.Activator;
+import net.sf.ecl1.utilities.logging.ICommonLogger;
+import net.sf.ecl1.utilities.logging.LoggerFactory;
+
+/**
+ * SWT-related utility class.
+ */
+public class SwtUtil {
+
+    private static final ICommonLogger logger = LoggerFactory.getLogger(Activator.class.getSimpleName(), Activator.PLUGIN_ID, Activator.getDefault());
+
+    /**
+     * Brings the active shell to the foreground by minimizing and then maximizing it.
+     * This ensures the shell becomes the active window, even if it was previously in the background.
+     *
+     * @param display the Display instance, which is used to get the active shell.
+     */
+    public static void bringShellToForeground(Display display) {
+        if(display == null){
+            logger.error("Error bringing Shell to foreground, Display is null");
+            return;
+        }
+        display.asyncExec(() -> {
+            if (!display.getActiveShell().getMinimized()){
+                display.getActiveShell().setMinimized(true);
+            }
+            display.getActiveShell().setMinimized(false);
+        });
+    }
+}
