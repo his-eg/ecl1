034882253c687d2f626106d9a4c3978755a42fc5
Lukas Hitzemann <45481788+Batschko@users.noreply.github.com>
2025-03-17 12:22:04 +0100

    #336831: Add App class to run AutoLfsPruneStarter

---
+++ net.sf.ecl1.git.auto-lfs-prune/src/net/sf/ecl1/git/auto/lfs/prune/standalone/AutoLfsPruneStarterApp.java
@@ -0,0 +1,11 @@
+package net.sf.ecl1.git.auto.lfs.prune.standalone;
+
+import net.sf.ecl1.git.auto.lfs.prune.AutoLfsPruneStarter;
+
+public class AutoLfsPruneStarterApp {
+
+	public static void main(String[] args) {
+		new AutoLfsPruneStarter().earlyStartup();
+	}
+
+}
