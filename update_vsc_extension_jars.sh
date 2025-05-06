9f258b30c7309e7a09f398436365be7b95706cda
Lukas Hitzemann <45481788+Batschko@users.noreply.github.com>
2025-05-06 10:31:40 +0200

    #336831: Ensure jars directory is created

--- update_vsc_extension_jars.sh
+++ update_vsc_extension_jars.sh
@@ -12,6 +12,8 @@ rm -rf "$VSCODE_LIB"/*
 # Find all Ecl1 JAR files, excluding updatecheck, since VSCode handles updates automatically
 FILES=$(find . -type f -path "*/build/libs/*-all.jar"  ! -name "net.sf.ecl1.updatecheck-all.jar")
 
+mkdir -p "$VSCODE_LIB"
+
 for FILE in $FILES; do
     echo "Copying $FILE to $VSCODE_LIB"
     cp "$FILE" "$VSCODE_LIB/"
