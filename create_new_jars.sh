b519573a5acc4568c00d84bb0bf822c6b51bcba8
Simon Sohrt <sohrt@his.de>
2025-10-16 14:43:34 +0200

    #355991: Updated the build scripts

--- create_new_jars.sh
+++ create_new_jars.sh
@@ -9,9 +9,8 @@ rm ./h1updatesite/plugins/*
 echo "############################"
 echo "You now need to open ecl1 with eclipse (and installed PDE) and do the following steps:"
 echo "1. Mark all projects and refresh them"
-echo "2. File -> Export -> Plug-in Development -> Deployable features"
-echo "3. Choose the \"updatesite\" folder as the directoy"
-echo "4. Export"
+echo "2. Open ./h1updatesite/site.xml"
+echo "3. Press the button \"Build All\""
 echo "Please press enter when you have created the new jars"
 echo "############################"
 read 
